69ec0341974fdd81dde1c3801915f75c9ea3bf7f
punytan <punytan@gmail.com>
2011-10-18 02:36:48 +0900

    initial commit

---
+++ lib/Config/PP.pm
@@ -0,0 +1,32 @@
+package Config::PP;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Config::PP -
+
+=head1 SYNOPSIS
+
+  use Config::PP;
+
+=head1 DESCRIPTION
+
+Config::PP is
+
+=head1 AUTHOR
+
+punytan E<lt>punytan@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
