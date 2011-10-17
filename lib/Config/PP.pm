3e5b3c9e953715f06eb908cae588f4759c402a89
punytan <punytan@gmail.com>
2011-10-18 06:15:47 +0900

    impl

--- lib/Config/PP.pm
+++ lib/Config/PP.pm
@@ -1,7 +1,43 @@
 package Config::PP;
 use strict;
 use warnings;
+
+use parent 'Exporter';
+our @EXPORT  = qw(config_get config_set);
+
+use Data::Dumper;
+use File::Spec;
+use Carp;
+
 our $VERSION = '0.01';
+our $DIR     = File::Spec->catfile($ENV{HOME}, '.ppconfig');
+
+# path: $DIR/${namespace}.pl
+
+sub config_get ($) {
+    my $namespace = shift;
+    my $path = path($namespace);
+    return (do $path or Carp::croak "$!: $path");
+}
+
+sub config_set ($$) {
+    my ($namespace, $data) = @_;
+    my $path = path($namespace);
+
+    open my $fh, ">", $path or Carp::croak "$!: $path";
+    print {$fh} Dumper($data);
+    close $fh or Carp::croak $!;
+}
+
+sub path {
+    my $namespace = shift;
+
+    unless (-d $DIR) {
+        mkdir $DIR, 0700 or Carp::croak "Can't mkdir: $DIR";
+    }
+
+    File::Spec->catfile($DIR, "${namespace}.pl");
+}
 
 1;
 __END__
@@ -13,6 +49,11 @@ Config::PP -
 =head1 SYNOPSIS
 
   use Config::PP;
+
+  config_set "example.com", {hoge => 'fuga'};
+
+  print Dumper config_get "example.com";
+
 
 =head1 DESCRIPTION
 
