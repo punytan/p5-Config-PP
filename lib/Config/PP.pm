911516456e8b9f048a1e74b4e173d237f2417b70
punytan <punytan@gmail.com>
2011-10-19 00:02:26 +0900

    documentation

--- lib/Config/PP.pm
+++ lib/Config/PP.pm
@@ -44,26 +44,50 @@ __END__
 
 =head1 NAME
 
-Config::PP -
+Config::PP - lightweight configuration file manager that uses Pure Perl to serialize.
 
 =head1 SYNOPSIS
 
-  use Config::PP;
+  use Config::PP; # exports config_set() and config_get()
 
-  config_set "example.com", {hoge => 'fuga'};
+  config_set "example.com", {
+    email    => 'foo@example.com'
+    password => 'barbaz'
+  };
 
   print Dumper config_get "example.com";
 
-
 =head1 DESCRIPTION
 
-Config::PP is
+Config::PP is lightweight configuration file manager that uses Pure Perl to serialize.
+
+=head1 FUNCTIONS
+
+C<config_set()> and C<config_get()> throw exception on error.
+
+=head2 config_set $namespace, $configuration
+
+Saves configuration file into C<"$Config::PP::DIR/$namespace.pl">.
+
+=head2 config_get $namespace
+
+Loads configuration from C<"$Config::PP::DIR/$namespace.pl">.
+
+=head1 Configuration Variables
+
+=head2 C<$Config::PP::DIR>
+
+Default directory is C<"$ENV{HOME}/.ppconfig">.
+
+You can configure the path to save configuration file.
 
 =head1 AUTHOR
 
 punytan E<lt>punytan@gmail.comE<gt>
 
 =head1 SEE ALSO
+
+L<Config::Pit>
 
 =head1 LICENSE
 
